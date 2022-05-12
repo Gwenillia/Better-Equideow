5329cbed95ddf090cc5dab46d0c59332703d1127
Gwenillia <gwenden31@gmail.com>
2022-05-12 14:41:27 +0200

    add blup display on Horse Sales and fix blank spaces

--- moreInfos.js
+++ moreInfos.js
@@ -1,6 +1,14 @@
 function moreInfos() {
   const names = document.getElementsByClassName('horsename');
   const namesArr = Array.from(names);
+  // const affixes = document.getElementsByClassName('affixe');
+
+  // Array.from(affixes).forEach((affixe) => {
+  //   const parentNode = affixe.parentNode;
+  //   const br = parentNode.children[1];
+
+  // parentNode.removeChild(br);
+  // })
 
   const regexpBlupHtml = /<td class="last align-right" width="15%" dir="ltr"><strong class="nowrap">[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?<\/strong><\/td>/;
   const regexpPGHtml = /<strong>Total.+[+-]?(?=\d*[.eE])(?=\.?\d)\d*\.?\d*(?:[eE][+-]?\d+)?<\/strong>/;
@@ -26,15 +34,16 @@ function moreInfos() {
         infoDiv.style.margin = '.5em 0';
         infoDiv.style.color = '#993322';
 
-        if (window.location.href.indexOf('elevage/chevaux/?elevage') > -1) {
+        if (window.location.href.indexOf('elevage/chevaux/?elevage') > -1 || window.location.href.indexOf('marche/vente') > -1) {
           const blupHtml = data.match(regexpBlupHtml);
           if (blupHtml) {
             const blupFloat = blupHtml[0].match(regexpFloat);
             infoDiv.innerHTML += '<span><span style="font-weight: bold;">Blup: </span>' + blupFloat[0] + '</span>';
           }
 
-          if (!isDetailedView) {
+          if (!isDetailedView && !(window.location.href.indexOf('marche/vente') > -1)) {
             const PGHtml = data.match(regexpPGHtml);
+            console.log(!(window.location.href.indexOf('marche/vente') > -1));
             if (PGHtml) {
               const PGFloat = PGHtml[0].match(regexpFloat);
               infoDiv.innerHTML += `<span><span style="font-weight: bold;">${isFrenchApp ? 'PG: ' : 'GP: '}</span>${PGFloat[0]}</span>`;
@@ -49,10 +58,9 @@ function moreInfos() {
             infoDiv.innerHTML += `<span><span style="font-weight: bold;">${isFrenchApp ? 'Compétences: ' : 'Skills: '}</span>${skillsFloat[0]}</span>`;
           }
         }
-
-        if (window.location.href.indexOf('elevage/chevaux/?elevage') > -1 || window.location.href.indexOf('centre/box') > -1) {
-          name.parentNode.insertBefore(infoDiv, name.nextSibling);
-        }
+        name.parentNode.insertBefore(infoDiv, name.nextSibling);
+        const br = name.parentNode.children[2]
+        name.parentNode.removeChild(br);
       });
   });
 }
