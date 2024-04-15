eaab1fdf6fd3cc85e1802432c548ffa8ba36be8b
Andrea Méndez Sanz <andrea.mendez.sanz01@gmail.com>
2024-04-15 11:33:15 +0200

    Added spanish translation

--- translation.js
+++ translation.js
@@ -112,6 +112,35 @@ class Translation {
           pg: 'GP: ',
           skills: 'Skills: '
         }
+      },
+      ,
+      es: {
+        stat: {
+          stamina: 'resistencia',
+          speed: 'velocidad',
+          dressage: 'doma',
+          gallop: 'galope',
+          trot: 'trote',
+          jumping: 'salto'
+        },
+        competition: {
+          trot: 'Trote',
+          gallop: 'Galope',
+          dressage: 'Doma',
+          crossCountry: 'Cross',
+          showJumping: 'Carrera de obstaculos',
+          barrelRacing: 'Barrel racing',
+          cutting: 'Cutting',
+          trailClass: 'Trail class',
+          reining: 'Reining',
+          westernPleasure: 'Western pleasure',
+          disclaimer: 'Cuanto mayores sean las habilidades, mayores serán las posibilidades de ganar'
+        },
+        other: {
+          pet: 'Mascota: ',
+          pg: 'PG: ',
+          skills: 'Habilidades: '
+        }
       }
     }
   }
@@ -122,7 +151,8 @@ class Translation {
     const languageMap = {
       'equideow': 'fr',
       'howrse.no': 'no',
-      'howrse.pl': 'pl'
+      'howrse.pl': 'pl',
+      'caballow.com': 'es'
     };
 
     for (let domain in languageMap) {
